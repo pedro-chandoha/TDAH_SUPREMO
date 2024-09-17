7130df5c23ce5208ba8504eea632a3c8c4d605ec
pedro-chandoha <160602791+pedro-chandoha@users.noreply.github.com>
2024-09-17 08:05:36 -0300

    calculo imc de paulo

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,7 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
+var paulo = document.querySelector('#paciente1');
+var peso = paulo.querySelector('.info-peso');
+var altura = paulo.querySelector('.info-altura');
+var imc = peso / (altura*altura);
+document.querySelector('.info-imc').textContent = imc;
