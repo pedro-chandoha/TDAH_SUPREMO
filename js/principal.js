32d80021870dd15da263a33b0ef992acc6d22d65
pedro-chandoha <160602791+pedro-chandoha@users.noreply.github.com>
2024-09-17 08:35:56 -0300

    ty

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,7 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
 var paulo = document.querySelector('#paciente1');
-var peso = paulo.querySelector('.info-peso').textContent;
-var altura = paulo.querySelector('.info-altura').textContent;
-var imc = peso / (altura*altura);
-paulo.querySelector('.info-imc').textContent = imc;
+var peso = paulo.querySelector(".info-peso").textContent;
+var altura = paulo.querySelector(".info-altura").textContent;
+var imc = peso / (altura * altura);
+paulo.querySelector(".info-imc").textContent = imc;
