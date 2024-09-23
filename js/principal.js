45527b3c1bea2283dcdf322e2f011ffb9b8cd7b7
pedro-chandoha <160602791+pedro-chandoha@users.noreply.github.com>
2024-09-23 11:32:52 -0300

    calculo de imc

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,15 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = "Aparecida Nutricionista";
-var paulo = document.querySelector('#paciente1');
-var peso = paulo.querySelector(".info-peso").textContent;
-var altura = paulo.querySelector(".info-altura").textContent;
-var imc = peso / (altura * altura);
-paulo.querySelector(".info-imc").textContent = imc;
+//var paulo = document.querySelector('#paciente1');
+// var peso = paulo.querySelector(".info-peso").textContent;
+// var altura = paulo.querySelector(".info-altura").textContent;
+// var imc = peso / (altura * altura);
+// paulo.querySelector(".info-imc").textContent = imc;
+
+var pacientes = document.querySelectorAll('.paciente');
+for(var contador = 0;contador < pacientes.length; contador++ ){
+    var peso = pacientes[contador].querySelector('.info-peso').textContent;
+    var altura = pacientes[contador].querySelector('.info-altura').textContent;
+    var imc = peso / ( altura * altura );
+    pacientes[contador].querySelector('.info-imc').textContent = imc;
+}
